658315fb5a5cab3ac9754eea4ec9ded9135b4d65
VISHWAS76 <140800019+vishwasp1005@users.noreply.github.com>
2026-03-28 12:16:12 +0530

    Update apiClient.js

--- quiz-frontend/src/services/apiClient.js
+++ quiz-frontend/src/services/apiClient.js
@@ -6,7 +6,7 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-    baseURL: "http://127.0.0.1:8000",
+    baseURL: "https://dl-project-pdf-to-mcq.onrender.com",
 });
 
 apiClient.interceptors.request.use((req) => {
